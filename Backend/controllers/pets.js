6a0247ecfa84e7104c7997ee408d94d213fbc209
ElAndy94 <andrewpeliza@hotmail.com>
2020-03-13 13:59:05 +0000

    added controller updated commands

--- Backend/controllers/pets.js
+++ Backend/controllers/pets.js
@@ -7,39 +7,85 @@ AWS.config.credentials = new AWS.CognitoIdentityCredentials({
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 exports.getMovies = (req, res) => {
-  const table = "Movies";
-  let infoArray = [];
+  //   const table = "Movies";
+  //   let infoArray = [];
+  //   const params = {
+  //     TableName: table,
+  //     ProjectionExpression: "#yr, title, info.rating",
+  //     FilterExpression: "#yr between :start_yr and :end_yr",
+  //     ExpressionAttributeNames: {
+  //       "#yr": "year"
+  //     },
+  //     ExpressionAttributeValues: {
+  //       ":start_yr": 2001,
+  //       ":end_yr": 2020
+  //     }
+  //   };
+  //   dynamoDB.scan(params, (err, data) => {
+  //     if (err) {
+  //       console.log(
+  //         "Unable to scan the table. Error JSON:",
+  //         JSON.stringify(err, null, 2)
+  //       );
+  //     } else {
+  //       data.Items.forEach(movie => {
+  //         info = {
+  //           Year: movie.year,
+  //           Title: movie.title,
+  //           Rating: movie.info.rating
+  //         };
+  //         infoArray.push(info);
+  //         infoArray.push(info);
+  //       });
+  //       res.send(infoArray);
+  //     }
+  //   });
+};
 
-  const params = {
+exports.insertPet = async (req, res) => {
+  var table = "Pets";
+
+  var petID =
+    Math.random()
+      .toString(36)
+      .substring(2, 15) +
+    Math.random()
+      .toString(36)
+      .substring(2, 15);
+  var petType = "Dog";
+  var petBreed = "Pug";
+  var age = Math.random() * 10;
+  var description = "Safe";
+
+  var params = {
     TableName: table,
-    ProjectionExpression: "#yr, title, info.rating",
-    FilterExpression: "#yr between :start_yr and :end_yr",
-    ExpressionAttributeNames: {
-      "#yr": "year"
-    },
-    ExpressionAttributeValues: {
-      ":start_yr": 2001,
-      ":end_yr": 2020
+    Item: {
+      PetID: petID,
+      PetType: petType,
+      PetBreed: petBreed,
+      info: {
+        age: age,
+        description: description
+      }
     }
   };
 
-  dynamoDB.scan(params, (err, data) => {
+  await dynamoDB.put(params, err => {
     if (err) {
-      console.log(
-        "Unable to scan the table. Error JSON:",
+      console.error(
+        "Unable to add item. Error JSON: ",
         JSON.stringify(err, null, 2)
       );
-    } else {
-      data.Items.forEach(movie => {
-        info = {
-          Year: movie.year,
-          Title: movie.title,
-          Rating: movie.info.rating
-        };
-        infoArray.push(info);
-        infoArray.push(info);
-      });
-      res.send(infoArray);
     }
+    return setTimeout(() => {
+      console.log("bam");
+    }, 2000);
+    // } else {
+    // return data;
+    //res.send("Added Item:", JSON.stringify(data, null, 2));
+    // console.log("Added Item:", JSON.stringify(data, null, 2));
+    // }
   });
+  console.log("harri");
+  res.send("it worked?");
 };
--- Backend/routes/pets.js
+++ Backend/routes/pets.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const PetController = require("../controllers/pets");
 
 router.get("", PetController.getMovies);
+
 router.post("/insert", PetController.insertPet);
 
 module.exports = router;
